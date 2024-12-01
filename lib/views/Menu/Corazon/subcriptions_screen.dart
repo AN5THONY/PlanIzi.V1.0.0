dacefdb37914a9845abfe32e323f6b3bd0e4529a
5anitos <anthobn4@gmail.com>
2024-12-01 13:59:49 -0500

    Cambios brutales que tal vez su pc se le crashee

--- lib/views/Menu/Corazon/subcriptions_screen.dart
+++ lib/views/Menu/Corazon/subcriptions_screen.dart
@@ -1,14 +1,51 @@
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:plan_izi_v2/theme/app_colors.dart';
+import 'package:plan_izi_v2/models/bag.dart';
 
 
-class SubcriptionsScreen extends StatelessWidget {
+class SubcriptionsScreen extends StatefulWidget {
   const SubcriptionsScreen({super.key});
 
   @override
+  State<SubcriptionsScreen> createState() => _SubcriptionsScreenState();
+}
+
+class _SubcriptionsScreenState extends State<SubcriptionsScreen> {
+  List<Bag> bags = listOfBags();
+  List<String> heroImages = [
+    "https://static.mercadonegro.pe/wp-content/uploads/2022/07/22162714/Iconico-IncaKola-2021-1-scaled.jpg",
+    "https://www.heyhunters.com/wp-content/uploads/2022/07/web-HH-IK-IG-behance2-01.jpg",
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("SUSCRIPCIONES", style: TextStyle( fontSize: 24, color: AppColors.textPrimary, )),
+    return Column(
+      children: [
+        Stack(
+          children: [
+            CarouselSlider.builder(
+              itemCount: heroImages.length,
+              options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                enlargeCenterPage: true,
+              ),
+              itemBuilder: (context, index, realIndex) {
+                return Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(heroImages[index]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                );
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
