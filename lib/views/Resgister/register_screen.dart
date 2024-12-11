079afa8cbd8b911fd6332f9988d0abda5bd8d496
AN5THONY <142169138+AN5THONY@users.noreply.github.com>
2024-12-11 13:23:49 -0500

    bton de josué
    xd

--- lib/views/Resgister/register_screen.dart
+++ lib/views/Resgister/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:plan_izi_v2/theme/app_colors.dart';
 import 'package:plan_izi_v2/views/Login/login_screen.dart';
 import 'package:plan_izi_v2/views/Recovery/recovery_screen.dart';
@@ -7,19 +8,31 @@ import 'package:plan_izi_v2/widgets/dropdown/custom_dropdown.dart';
 import 'package:plan_izi_v2/widgets/textfields/custom_textfield.dart';
 
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
 
+
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController nameController = TextEditingController();
+
   final TextEditingController lastNameController = TextEditingController();
+
   final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
+
   final TextEditingController confirmController = TextEditingController();
 
   final List<String> ageoptions = List.generate(100, (index) => "${index + 1}");
+
   final List<String> genderOptions = ["Masculino", "Femenino", "Otro genero"];
 
-
-  RegisterScreen({super.key});
+  DateTime? selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +51,39 @@ class RegisterScreen extends StatelessWidget {
               const SizedBox(height: 20,),
               CustomTextfield(controller: lastNameController, labelText: 'Introduce tus apellidos'),
               const SizedBox(height: 10,),
-              CustomDropdown(hintText: "Edad", items: ageoptions),//+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('Fecha de Nacimiento ', style: TextStyle(fontSize:16, color: AppColors.textPrimary )),
+                  const SizedBox(width: 40,),
+                  ElevatedButton(
+                  style: ElevatedButton.styleFrom( 
+                    iconColor: AppColors.primary,
+                    surfaceTintColor: Colors.black,
+                    foregroundColor: AppColors.third,
+                    disabledIconColor: AppColors.third,
+                    elevation: 5,
+                    shadowColor: const Color.fromARGB(255, 83, 83, 83),
+                    
+                  ),
+                  onPressed: () async {
+                    selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1950),
+                      lastDate: DateTime.timestamp(),
+
+                    );
+                    setState(() {});
+                  },
+                  child: Text(
+                      selectedDate == null
+                        ? "Seleccionar fecha"
+                        : DateFormat('yyyy-MM-dd').format(selectedDate!),
+                                              ),
+                  )
+                ],
+              ),
               CustomDropdown(hintText: "Género", items: genderOptions),//+
               const SizedBox(height: 10,),
               CustomTextfield(
